dd055442e1e19f1ceb7546b4c878d9eedd2b7965
Roberto <roberto.ht2ml@gmail.com>
2021-07-28 19:47:41 -0300

    links na home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,28 +6,34 @@
         
         <div class="card-deck">
             <div class="card">
-              <img class="card-img-top" src="{{ asset('img/guias.png')}}" alt="Card image cap">
+              <img class="rounded mx-auto d-block icon_home" src="{{ asset('img/guias.png')}}" alt="Card image cap">
               <div class="card-body">
                 <h5 class="card-title">Guias</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
               </div>
+              <div class="card-footer">
+                <p class="card-text"><a href="{{ route('guias.index')}}" class="btn btn-primary btn-block">Ver guias</a></p>
+            </div>
             </div>
             <div class="card">
-              <img class="card-img-top" src="{{ asset('img/roteiros_icon.png')}}" alt="Card image cap">
+              <img class="rounded mx-auto d-block icon_home" src="{{ asset('img/roteiros_icon.png')}}" alt="Card image cap">
               <div class="card-body">
                 <h5 class="card-title">Roteiros</h5>
                 <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
               </div>
+              <div class="card-footer">
+                <p class="card-text"><a href="{{ route('roteiros.index')}}" class="btn btn-primary btn-block">Ver os roteiros</a></p>
+            </div>
             </div>
             <div class="card">
-              <img class="card-img-top" src="{{ asset('img/favoritos.png')}}" alt="Card image cap">
+              <img class="rounded mx-auto d-block icon_home" src="{{ asset('img/favoritos.png')}}" alt="Card image cap">
               <div class="card-body">
                 <h5 class="card-title">Meus Favoritos</h5>
                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
               </div>
+              <div class="card-footer">
+                <p class="card-text"><a href="{{ route('guias.index')}}" class="btn btn-primary btn-block">Ver meus favoritos</a></p>
+            </div>
             </div>
           </div>
 
