84f64753c0fec82574f203261788e6fe03446546
Roberto <roberto.ht2ml@gmail.com>
2021-08-13 22:50:05 -0300

    subindo correção de segurança

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,13 @@
     <meta charset="utf-8">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
+
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
 
     <title>e_traslados</title>
 
